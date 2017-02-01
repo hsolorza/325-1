7c8a8907a82e6b2672387ccf773a6ffe864fe525
hsolorza <hsolorzano@osuosl.org>
2017-02-01 14:02:50 -0800

    Add Bruteforce
    Adds the brute force project

---
+++ Bruteforce/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * Analysis of Algorithms- Program 1(Brute Force)
+ * Purpose: Calculate the least distance between two points
+ *          in a list of points using a bruteforce method
+ * @author Hannah Solorzano
+ * @version 1/29/17
+ *
+ * */
+
+#include <iostream>
+#include <fstream>
+#include "points.h"
+#include <utility>
+
+using namespace std;
+
+int main()
+{
+
+    Points p;
+    p.insert();
+
+    p.findShortestPath();
+
+
+    return 0;
+}
+
